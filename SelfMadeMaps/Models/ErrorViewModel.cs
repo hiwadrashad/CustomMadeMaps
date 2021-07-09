c0890960b9f2b97dd19854f9380ccd2a38c6dcb8
hiwad rashad <hiwadrashad112@hotmail.com>
2021-07-09 15:56:47 +0200

    Add project files.

---
+++ SelfMadeMaps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SelfMadeMaps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
